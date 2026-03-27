d5b8593289549c0c7e13e9066f224f5abe1c8838
lucamalcomartens <martens.lucamalco@gmx.de>
2026-03-27 10:58:39 +0100

    fix: Correct RMSE calculation in model evaluation function

--- features/predictions/modeling.py
+++ features/predictions/modeling.py
@@ -24,7 +24,7 @@ def evaluate_model(model, X_test, y_test):
 
     y_pred = model.predict(X_test)
 
-    rmse = mean_squared_error(y_test, y_pred)
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
     mae = mean_absolute_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)
     signs_correct = np.sum(np.sign(y_test) == np.sign(y_pred))
